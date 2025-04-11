3742f8295fef5c8c8123f87152b81eb3ec3140e1
KITAKA GERALD <88924569+KITAKAGERALD@users.noreply.github.com>
2025-04-11 12:05:36 +0000

    finals

--- services/quotes.js
+++ services/quotes.js
@@ -4,51 +4,46 @@ const config = require('../config');
 function getMultiple(page = 1) {
   const offset = (page - 1) * config.listPerPage;
   const data = db.query(`SELECT * FROM quote LIMIT ?,?`, [offset, config.listPerPage]);
-  const meta = {page};
+  const meta = { page };
 
   return {
     data,
     meta
   }
 }
-function validateCreate(quote)
-{
-    let messages = [];
-    console.log(quote);
-    if(!quote)
-    {
-      messages.push(`NO QUOTE IS PROVIDED`);
+function validateCreate(quote) {
+  let messages = [];
+  console.log(quote);
+  if (!quote) {
+    messages.push(`NO QUOTE IS PROVIDED`);
 
-    }
-    else if(!quote.quote)
-  {
+  }
+  else if (!quote.quote) {
     messages.push(`Quote is empty`);
   }
   if (!quote.author) {
     messages.push('Author is empty');
   }
 
-    if (messages.length)
-    
-    {
+  if (messages.length) {
     let error = new Error(messages.join());
     error.statusCode = 400;
 
     throw error;
-    }
+  }
 }
 
 
-function create(quoteObj){
+function create(quoteObj) {
   validateCreate(quoteObj);
 
-  const {quote, author } = quoteObj;
-const result = db.run('INSERT INTO quote (quote, author) VALUES (@quote, @author)', {quote, author});
-let message ="Error occured";
-  if(result.changes) {
-    message ="QUOTE CREATED!";
-  } 
-  return {message};
+  const { quote, author } = quoteObj;
+  const result = db.run('INSERT INTO quote (quote, author) VALUES (@quote, @author)', { quote, author });
+  let message = "Error occured";
+  if (result.changes) {
+    message = "QUOTE CREATED!";
+  }
+  return { message };
 }
 
 
